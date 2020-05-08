da2e5104b329f5c616f03fd07562112d0c70bb44
acornwe3 <amanda@MSEL-LIAPMAC06.fios-router.home>
2020-05-08 11:49:58 -0400

    draft ecs modules

---
+++ ecs-lb/main.tf
@@ -0,0 +1,95 @@
+resource "aws_ecs_service" "service" {
+  name = format("%s-%s-%s", var.project_name, var.environment, var.component)
+  cluster = var.cluster_id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+
+  desired_count = 1
+
+  deployment_maximum_percent = 100
+  deployment_minimum_healthy_percent = 0
+
+  load_balancer {
+    target_group_arn  = var.lb_target_group_arn 
+    container_port    = var.port 
+    container_name    = var.component
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.service_discovery.arn
+  }
+
+  network_configuration {
+    security_groups = [ var.subnet_egress_id, var.subnet_component_id, var.subnet_modules_id ]
+    subnets         = var.subnet_nat_id
+  }
+
+  propagate_tags = "SERVICE"
+
+  tags = {
+    Department = "LAG"
+    Project = var.project_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_cloudwatch_log_group" "log" {
+  name              = format("msel_lags-%s-%s", var.log_prefix, var.component)
+  retention_in_days = 1
+}
+
+locals {
+  db_host = format("db-%s.%s-local", var.component, var.environment)
+}
+
+resource "aws_ecs_task_definition" "task_definition" {
+  family = format("%s-%s-%s", var.project_name, var.environment, var.component)
+  network_mode = "awsvpc"
+  container_definitions = <<EOF
+  [
+    {
+      "name": "${var.component}",
+      "image": "${var.image}",
+      "portMappings": [
+        {
+          "containerPort": ${var.port},
+          "hostPort": ${var.port}      
+        }
+      ],
+      "environment": [{
+        "name": "environment",
+        "value": "${var.environment}"
+      },
+      ${var.environment_json}],
+      "memory": ${var.memory},
+      "cpu": ${var.cpu},
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-region": "${var.region}",
+          "awslogs-group": "${aws_cloudwatch_log_group.log.name}",
+          "awslogs-stream-prefix": "${var.log_prefix}"
+        }
+      }
+    }
+  ]
+EOF
+}
+
+resource "aws_service_discovery_service" "service_discovery" {
+  name = format("%s-%s-%s", var.project_name, var.environment, var.component)
+
+  dns_config {
+    namespace_id = var.dns_service_namespace
+
+    dns_records {
+      ttl  = 10
+      type = "A" #A for awsvpc SRV for bridge
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
---
+++ ecs-lb/variables.tf
@@ -0,0 +1,84 @@
+
+variable "vpc_id" {
+  description = "id for vpc"
+}
+
+variable "region" {
+  description = "AWS region"
+}
+
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  description = "env prefix"
+}
+
+######################## ecs service/task ##############################
+
+variable "component" {
+  description = "name of the ecs component"
+}
+
+
+variable "environment_json" {
+  description = "name of the ecs component"
+}
+
+variable "image" {
+  description = "location of the container for the task definition"
+}
+
+variable "port" {
+  description = "port for container"
+  default = 3000
+  type = number
+}
+
+variable "log_prefix" {
+  description = "prepend log"
+}
+
+variable "memory" {
+  description = "memory for task definition"
+  type = number
+}
+
+
+variable "cpu" {
+  description = "cpu for task definition"
+  type = number
+}
+
+variable "lb_target_group_arn" {
+  description = "arn for the load balancer target group"
+}
+
+
+######################## networking ##############################
+
+variable "cluster_id" {
+  description = "id for vpc"
+}
+
+variable "subnet_nat_id" {
+  description = "id for vpc"
+}
+
+variable "subnet_egress_id" {
+  description = "id for vpc"
+}
+
+variable "subnet_component_id" {
+  description = "id for vpc"
+}
+
+variable "subnet_modules_id" {
+  description = "id for vpc"
+}
+
+variable "dns_service_namespace" {
+  description = "id for vpc"
+}
